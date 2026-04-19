18e7d94822322083dbe926ab47e96b8d4a54f9de
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-04-19 19:28:51 +0545

    made models for the backend. Starting to work on backend

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
                 <DropdownMenuContent className="w-56" align="end">
                   <DropdownMenuLabel className="font-normal">
                     <div className="flex flex-col space-y-1">
-                      <p className="text-sm font-medium leading-none">
+                      <p className="text-sm font-bold leading-none">
                         {session.user.name}
                       </p>
                       <p className="text-xs leading-none text-muted-foreground">
